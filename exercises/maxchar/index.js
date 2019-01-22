a713d90149d629bf5c3cf943f98315f355003491
Matthew Philps <philps.matthew@gmail.com>
2019-01-23 08:21:17 +0900

    maxchar refactored

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -6,26 +6,43 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-    const strArray = str.split('')
     let strObject = {}
     let count = 0
     let keyName = undefined
-    strArray.forEach((s)=>{
-        if(!strObject[s]){
-            strObject[s] = 1
-        } else {
-            strObject[s] ++
-        }   
-    })
+
+    for(char of str){
+        strObject[char] ? strObject[char]+1 : strObject[char] = 1
+    }
+    
     for(key in strObject){
-        // console.log(strObject)
         if(strObject[key]>count){
             count = strObject[key]
             keyName = key
-            console.log(count, key)
         }
     }
     return keyName
 }
+
+//first attempt
+// function maxChar(str) {
+//     const strArray = str.split('')
+//     let strObject = {}
+//     let count = 0
+//     let keyName = undefined
+//     strArray.forEach((s)=>{
+//         if(!strObject[s]){
+//             strObject[s] = 1
+//         } else {
+//             strObject[s] ++
+//         }   
+//     })
+//     for(key in strObject){
+//         if(strObject[key]>count){
+//             count = strObject[key]
+//             keyName = key
+//         }
+//     }
+//     return keyName
+// }
 
 module.exports = maxChar;
