29825a9d5340fe0b2c1d3ef1801af488747e02b2
Matthew Philps <philps.matthew@gmail.com>
2019-01-23 07:29:09 +0900

    completed reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,12 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let str = n+"";
+    let newNum = str.split("").reduce((rev, char)=>{
+        return char + rev
+    },"")
+    return newNum.includes("-") ? newNum.substring(0,newNum.length-1)*-1 : newNum*1
+}
 
 module.exports = reverseInt;
