748c0e368f45c9b3e633f4f73edf6ea01060e0cc
Matthew Philps <philps.matthew@gmail.com>
2019-01-23 08:05:46 +0900

    maxchar passed

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const strArray = str.split('')
+    let strObject = {}
+    let count = 0
+    let keyName = undefined
+    strArray.forEach((s)=>{
+        if(!strObject[s]){
+            strObject[s] = 1
+        } else {
+            strObject[s] ++
+        }   
+    })
+    for(key in strObject){
+        // console.log(strObject)
+        if(strObject[key]>count){
+            count = strObject[key]
+            keyName = key
+            console.log(count, key)
+        }
+    }
+    return keyName
+}
 
 module.exports = maxChar;
