5ac70e1844faafcfc1d102b8c7812a62803e04b8
Matthew Philps <philps.matthew@gmail.com>
2019-01-23 06:48:09 +0900

    refactored reverse string not to use native methods

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,11 +6,20 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// without using native methods
 function reverse(str) {
-    let strArray = str.split("")
-    strArray.reverse()
-    const result = strArray.join("")
-    return result 
+    let reversed = ''
+    for(char of str){
+        reversed = char + reversed
+    }
+    return reversed
 }
+
+// easy way
+// function reverse(str) {
+//     let strArray = str.split("")
+//     strArray.reverse()
+//     return strArray.join("")
+// }
 
 module.exports = reverse;
