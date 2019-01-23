530d3865bf4ada61cafcbc2096bff387f26df4be
Matthew Philps <philps.matthew@gmail.com>
2019-01-23 14:46:02 +0900

    refactored vowels to use regex

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -8,21 +8,28 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
-    const vowels = {
-        'a': 0,
-        'e':0,
-        'i':0,
-        'o':0,
-        'u':0
-    }
-    let count = 0
-    str = str.toLowerCase()
-    for (let char of str){
-        if (vowels[char] !== undefined){
-            count ++
-        }
-    }
-    return count
+    const matched = str.match(/[aeiou]/gi)
+    return matched ? matched.length : 0
 }
+
+// first attempt
+// function vowels(str) {
+//     const vowels = {
+//         'a': 0,
+//         'e':0,
+//         'i':0,
+//         'o':0,
+//         'u':0
+//     }
+//     let count = 0
+//     str = str.toLowerCase()
+//     for (let char of str){
+//         if (vowels[char] !== undefined){
+//             count ++
+//         }
+//     }
+//     return count
+// }
+
 
 module.exports = vowels;
