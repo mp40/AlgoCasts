2a2248f4bc7e6ca8146dac7605f197ec20b1ac8b
Matthew Philps <philps.matthew@gmail.com>
2019-01-24 09:26:40 +0900

    added for of loop using generator

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -125,6 +125,13 @@ class LinkedList {
             node = node.next
         }
     }
+    *[Symbol.iterator](){
+        let node = this.head
+        while(node){
+            yield node
+            node = node.next
+        }
+    }
 }
 
 module.exports = { Node, LinkedList };
